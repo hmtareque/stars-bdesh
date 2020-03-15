198291e3e8a179e6e8cee1826fb34dbea08226cc
Hasan Tareque <hasan@uxblondon.com>
2020-03-15 11:23:05 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            'stars' : [
+                    { 
+                        id: 'su',
+                        name: 'Sharif Uddin',
+                        email: '',
+                    },
+                    { 
+                        id: 'zb',
+                        name: 'Zahura Begum',
+                        email: '',
+                    },
+                    { 
+                        id: 'hmt1',
+                        name: 'Hasan Tareque',
+                        email: '',
+                    },
+                    { 
+                        id: 'ea',
+                        name: 'Eliza Ahmed',
+                        email: '',
+                    },
+                    { 
+                        id: 'hmt2',
+                        name: 'Hussain Taher',
+                        email: '',
+                    },
+                    { 
+                        id: 'ms',
+                        name: 'Mariam Sikkia',
+                        email: '',
+                    },
+                    { 
+                        id: 'hmt3',
+                        name: 'Hussain Tawhid',
+                        email: '',
+                    },
+                    { 
+                        id: 'hmt4',
+                        name: 'Hussain Taimim',
+                        email: '',
+                    },
+                ]
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+              { 
+              this.state.stars.map( stars => 
+              <div>{ stars.name }</div> 
+              )
+              }
+            </div>
+          );
+    }
 }
 
 export default App;
